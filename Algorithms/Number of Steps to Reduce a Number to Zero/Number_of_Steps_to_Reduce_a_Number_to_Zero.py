a705f9e6baa73cc183adaac9d76251640a68261b
Rome <thegoldenrain@gmail.com>
2020-02-12 23:45:46 -0800

    Create Number_of_Steps_to_Reduce_a_Number_to_Zero.py

---
+++ Algorithms/Number of Steps to Reduce a Number to Zero/Number_of_Steps_to_Reduce_a_Number_to_Zero.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numberOfSteps (self, num: int) -> int:
+        n = 0
+        
+        while num != 0:
+            if num % 2 == 0:
+                num = num / 2
+            else:
+                num = num - 1
+                
+            n = n + 1
+            
+        return n
