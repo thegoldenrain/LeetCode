b6da7d02c757ef29ad6738c6b7808dd9acd77139
Rome <thegoldenrain@gmail.com>
2020-02-13 00:33:34 -0800

    Create Number_of_Steps_to_Reduce_a_Number_to_Zero.java

---
+++ Algorithms/Number_of_Steps_to_Reduce_a_Number_to_Zero/Number_of_Steps_to_Reduce_a_Number_to_Zero.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfSteps (int num) {
+        int n = 0;
+        
+        while (num != 0){
+            if (num % 2 == 0)
+                num /= 2;
+            else
+                num -= 1;
+            
+            n += 1;
+        }
+        return n;
+    }
+}
