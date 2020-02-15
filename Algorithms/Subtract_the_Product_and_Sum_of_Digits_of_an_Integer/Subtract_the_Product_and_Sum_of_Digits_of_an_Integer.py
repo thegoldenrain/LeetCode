84a6544059da4396c2658d4dd4e8f9fb9c95ad7b
Rome <thegoldenrain@gmail.com>
2020-02-15 11:26:58 -0800

    Update Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py

--- Algorithms/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py
+++ Algorithms/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer/Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.py
@@ -4,8 +4,8 @@ class Solution:
         product = 1
         
         while n > 0:
-            sum = sum + (n % 10)
-            product = product * (n % 10)
-            n = n // 10
+            sum += (n % 10)
+            product *= (n % 10)
+            n //= 10
         
         return product - sum
