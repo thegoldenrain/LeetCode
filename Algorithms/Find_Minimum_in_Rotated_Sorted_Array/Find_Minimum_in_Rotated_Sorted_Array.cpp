244d4cf5f87dd11ebe9eb1ba6de71b4e741f3fee
Rome <thegoldenrain@gmail.com>
2020-02-21 23:29:48 -0800

    Create Find_Minimum_in_Rotated_Sorted_Array.cpp

---
+++ Algorithms/Find_Minimum_in_Rotated_Sorted_Array/Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int min = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] < min)
+                min = nums[i];
+        }
+        
+        return min;
+    }
+};
+    
