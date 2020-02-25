ec5dc0931a475e6760812fdaecc5b88e2318210e
Rome <thegoldenrain@gmail.com>
2020-02-25 15:35:04 -0800

    Update Find_Minimum_in_Rotated_Sorted_Array.py

--- Algorithms/Find_Minimum_in_Rotated_Sorted_Array/Find_Minimum_in_Rotated_Sorted_Array.py
+++ Algorithms/Find_Minimum_in_Rotated_Sorted_Array/Find_Minimum_in_Rotated_Sorted_Array.py
@@ -1,3 +1,9 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+        minNum = nums[0]
+        
+        for i in range(1, len(nums)):
+            if minNum > nums[i]:
+                minNum = nums[i]
+        
+        return minNum
